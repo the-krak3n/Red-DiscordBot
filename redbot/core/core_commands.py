ea6d00627a2461f5b1454a98bb023f6fb5298384
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-07-11 19:35:19 +0200

    `permissions` arg in `discord.utils.oauth_url` is now keyword-only

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -385,7 +385,7 @@ class CoreLogic:
         scopes = ("bot", "applications.commands") if commands_scope else None
         perms_int = data["invite_perm"]
         permissions = discord.Permissions(perms_int)
-        return discord.utils.oauth_url(app_info.id, permissions, scopes=scopes)
+        return discord.utils.oauth_url(app_info.id, permissions=permissions, scopes=scopes)
 
     @staticmethod
     async def _can_get_invite_url(ctx):
